4532bbbdbe5af4a618c83846ae23411ace9e1c20
VinecntLin <78530513+VinecntLin@users.noreply.github.com>
2024-03-22 00:20:16 +1100

    test puzzle

---
+++ Assets/Vincent/Script/NumBox.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumBox : MonoBehaviour
+{
+    public int index =0;
+    int x =0;
+    int y =0;
+
+    private Action<int, int> swapFunc=null;
+
+    public void Init(int i, int j, int index, Sprite sprite, Action<int,int> swapFunc){
+
+        this.index =index;
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
+        UpdatePos(i,j);
+        this.swapFunc =swapFunc;
+    }
+
+    public void UpdatePos(int i, int j){
+        x =i;
+        y =j;
+        this.gameObject.transform.localPosition = new Vector2(i,j);
+    }
+
+    public bool IsEmpty(){
+        // size of the  box prefab
+        return index ==16;
+    }
+
+
+    //swap the number
+    void OnMouseDown(){
+        if (Input.GetMouseButton(0) && swapFunc!= null){
+            swapFunc(x,y);
+        }
+    }
+}
