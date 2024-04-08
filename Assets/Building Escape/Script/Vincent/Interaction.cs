e7fedd733562829efa4ab27e78fb88935a68ad22
VinecntLin <78530513+VinecntLin@users.noreply.github.com>
2024-04-09 00:16:14 +1000

    Player Intercation

--- Assets/Building Escape/Script/Vincent/Interaction.cs
+++ Assets/Building Escape/Script/Vincent/Interaction.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera playerCamera; // Reference to the player's camera
+    [SerializeField] private GameObject targetObject;
+    public float interactionDistance = 3f; // Distance for interaction
+    [SerializeField] private KeyCode interactKey = KeyCode.F; // Key to interact
 
-    // Update is called once per frame
     void Update()
     {
-        
+    // Check if the interact key is pressed
+    Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+    RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)){
+            // Check if the hit object is the target object
+            if (hit.collider.gameObject == targetObject){
+                // Check if the player presses the 'F' key
+                if (Input.GetKeyDown(KeyCode.F)){
+                     // Do something when 'F' is pressed while looking at the target object
+                     Debug.Log("Player interacted with the target object.");
+                }
+            }
+        }
     }
 }
