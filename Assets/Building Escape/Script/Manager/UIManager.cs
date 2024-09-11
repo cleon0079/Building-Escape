bbfcd81004cf7bfbab1d9005f7b0f1edf6266faa
Jason-Zhouyw <80013758+Jason-Zhouyw@users.noreply.github.com>
2024-09-11 22:39:30 +1000

    UI Menu

--- Assets/Building Escape/Script/Manager/UIManager.cs
+++ Assets/Building Escape/Script/Manager/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 
 public class UIManager : MonoBehaviour
@@ -23,9 +24,14 @@ public class UIManager : MonoBehaviour
     }
     Manager manager;
     [SerializeField] private GameObject startPanel;
+    [SerializeField] private TMP_Text startText;
     [SerializeField] private Button startButton;
+    [SerializeField] private Button exitButton;
+    private GameInput inputs;
+    private InputAction exitKey;
     void Awake() 
     {
+    
         if (instance != null)
         {
             // there is already a UIManager in the scene, destory this one
@@ -36,7 +42,18 @@ public class UIManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-      
+       inputs = new GameInput();
+       exitKey = inputs.Player.Esc;
+       exitKey.started += SetMenuPanelOn;
+    }
+     private void OnEnable()
+    {
+        exitKey.Enable();
+        
+    }
+    private void OnDisable()
+    {
+        exitKey.Disable();
     }
 
     // Start is called before the first frame update
@@ -46,6 +63,7 @@ public class UIManager : MonoBehaviour
         manager.StartGame(true);
         startPanel.SetActive(true);
         startButton.onClick.AddListener(SetStartpanelOff);
+        exitButton.onClick.AddListener(manager.ExitGame);
     }
 
     // Update is called once per frame
@@ -55,8 +73,17 @@ public class UIManager : MonoBehaviour
     }
     void SetStartpanelOff()
     {
+        Time.timeScale = 1f;
         startPanel.SetActive(false);
         manager.StartGame(false);
-        Debug.Log("sdad");
     }
+
+    void SetMenuPanelOn(InputAction.CallbackContext context)
+    {
+        Time.timeScale = 0f;
+        startText.text = "Resume";  
+        startPanel.SetActive(true);
+        manager.StartGame(true);
+    }
+    
 }
